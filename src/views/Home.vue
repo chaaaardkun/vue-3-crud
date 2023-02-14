3260162ec179fe32fbcd2eb886194a1eb4ac143f
chad <chaaaardkun@gmail.com>
2023-02-14 13:05:36 +0800

    :sparkles: convert home page mobile responsive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,14 +6,14 @@ import Header from '../components/Header.vue';
 <template>
   <Header />
   <section class="grid lg:grid-cols-2 container mx-auto mt-32">
-    <div class="lg:max-w-md">
+    <div class="lg:max-w-md text-center lg:text-start">
       <h1>Lorem ipsum dolor sit amet, consectetur</h1>
-      <p class="mt-4">
+      <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
       </p>
 
-      <div class="flex gap-4 mt-4">
+      <div class="flex gap-4 mt-4 justify-center lg:justify-start">
         <button>Call to Action</button>
         <button
           class="bg-white border-primary text-primary hover:border-opacity-50"
@@ -37,7 +37,7 @@ import Header from '../components/Header.vue';
   </section>
 
   <section class="grid lg:grid-cols-3 container mx-auto lg:gap-16">
-    <div class="lg:max-w-xs">
+    <div class="lg:max-w-xs text-center lg:text-start">
       <h6>Header Text</h6>
       <p class="mt-4">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -51,8 +51,10 @@ import Header from '../components/Header.vue';
         class="bg-gray-300 relative mx-auto lg:mr-[5.625rem] w-[25rem] h-[25rem]"
       />
     </div>
-    <div class="flex flex-col justify-end lg:max-w-md">
-      <h6>Header Text</h6>
+    <div
+      class="flex flex-col justify-end lg:max-w-md text-center lg:text-start"
+    >
+      <h6 ckas>Header Text</h6>
       <p class="mt-4">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
         tempor incididunt ut labore et dolore magna aliqua.
@@ -60,28 +62,23 @@ import Header from '../components/Header.vue';
     </div>
   </section>
 
-  <section class="grid lg:grid-cols-3 lg:gap-16 bg-gray-100">
-    <div class="container mx-auto">
-      <div class="lg:max-w-xs">
-        <h6>Header Text</h6>
-        <p class="mt-4">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua.
-        </p>
-      </div>
-      <div class="relative w-full my-16 lg:my-0">
+  <section class="bg-gray-100 my-0 mt-32">
+    <div class="grid lg:grid-cols-2 py-32 lg:gap-16 container mx-auto">
+      <div class="relative w-full my-16 mx-auto lg:my-0 lg:-top-52">
         <img
           src="/"
           alt=""
-          class="bg-gray-300 relative mx-auto lg:mr-[5.625rem] w-[25rem] h-[25rem]"
+          class="bg-gray-300 mx-auto relative lg:absolute w-[35rem] h-[35rem]"
         />
       </div>
-      <div class="flex flex-col justify-end lg:max-w-md">
-        <h6>Header Text</h6>
+      <div class="lg:max-w-md text-center lg:text-start">
+        <h1>Lorem ipsum dolor sit amet, consectetur</h1>
         <p class="mt-4">
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
           eiusmod tempor incididunt ut labore et dolore magna aliqua.
         </p>
+
+        <button class="mx-auto lg:mx-0 mt-4">Call to Action</button>
       </div>
     </div>
   </section>
